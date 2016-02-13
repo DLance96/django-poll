ee9de7def9e791006fe26c9d098c5e5fc01bfe45
David Lance <dcl71@case.edu>
2016-02-13 04:23:50 -0500

    spellcheck errors primarily

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Question
 
 def index(request):
-    test_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
